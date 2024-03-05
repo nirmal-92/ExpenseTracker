6f20be7db84fc9244c09dbd9d2462812d3d0820d
nirmal-92 <nn3134395@gmail.com>
2024-03-05 12:03:27 +0530

    new commit

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const{Expense} = require('./schema.js')
 const app = express()
 app.use(bodyParser.json())
 
- async function connectToDb() {
+async function connectToDb() {
     try {
         await mongoose.connect('mongodb+srv://Nirmal:6192@cluster0.1sy5sx7.mongodb.net/ExpenseTracker?retryWrites=true&w=majority&appName=Cluster0)')
         const port = 8000
@@ -69,8 +69,35 @@ app.get('/get-expenses', async function(request,response){
     }
 })
 
+
+//localhost:8000/delete-expense/65e6aeebe6866b67ef03365a
+//localhost:8000/delete-expense/<params>
+app.delete('/delete-expense/:id',async function(request,response){
+    try{
+        const expenseData = Expense.findById(request.params.id)
+        if(expenseData){
+            await Expense.findByIdAndDelete(request.params.id)
+            response.status(200).json({
+                "status" : "success",
+                "message" : "deleted entry"
+            })
+        }
+        else{
+            response.status(404).json()({
+                "status" : "failure",
+                "message" : "could not find document"
+            })
+        }
+    }
+    catch(error){
+        response.status(404).json()({
+            "status" : "failure",
+            "message" : "could not find document"
+        })
+    }
+})
+
 // const port=8000
 // app.listen(port,function(){
 //     console.log(Listening on port  ${port}...)
 // })
-//mongodb+srv://Nirmal:6192@cluster0.1sy5sx7.mongodb.net/
